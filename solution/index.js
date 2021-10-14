ef2b5c915de068949715cc7c115900af854ec4b8
gugala <gugala@yandex.ru>
2021-10-14 11:03:23 +0300

    init

---
+++ solution/index.js
@@ -0,0 +1,23 @@
+module.exports = function (Homework) {
+
+    function promisify(callback, ...args) {
+        return new Promise(function(resolve, reject){ callback(...args, resolve)});
+    }
+
+    return (array, fn, initialValue, cb) => {
+        let result = initialValue;
+
+        while (true) {
+            let arrayLength = await promisify(array.length);
+            let stop = await promisify(equal, arrayLength, 0);
+            if (!stop) {
+                let popVal = await promisify(array.pop);
+                result = await promisify(fn, result, popVal, 0, array);
+            } else {
+                break;
+            }
+        }
+        
+        cb(result);
+    }
+}
